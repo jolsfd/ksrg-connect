d70c4881fd696e883a3026f445142cbe2777d738
jolsfd <76818674+jolsfd@users.noreply.github.com>
2022-04-06 16:43:52 +0200

    add type

--- backend/cmd/authentication.go
+++ backend/cmd/authentication.go
@@ -16,7 +16,7 @@ func HashPassword(password string) (string, error) {
 }
 
 // CheckPasswordHash compares if a password and a hash matchs.
-func CheckPasswordHash(password, hash string) bool {
+func CheckPasswordHash(password string, hash string) bool {
 	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
 	return err == nil
 }
